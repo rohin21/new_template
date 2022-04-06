fbd635ccef62ed3e6d66f4b19170bdc62bac0754
rohin <rohinpandey21@gmail.com>
2022-04-06 13:49:54 +0530

    api-done

--- main.py
+++ main.py
@@ -1,21 +1,50 @@
+import graphene
 from fastapi import FastAPI
 from starlette_graphene3 import GraphQLApp, make_graphiql_handler
 from starlette.applications import Starlette
 
-import graphene
+import models
+from db_conf import db_session
+from schemas import PostModel, PostSchema
+
+db = db_session.session_factory()
 
 app = Starlette()
+
+
+class Query(graphene.ObjectType):
+
+    all_posts = graphene.List(PostModel)
+    post_by_id = graphene.Field(PostModel, post_id=graphene.Int(required=True))
+
+    def resolve_all_posts(self, info):
+        query = PostModel.get_query(info)
+        return query.all()
+
+    def resolve_post_by_id(self, info, post_id):
+        return db.query(models.Post).filter(models.Post.id == post_id).first()
+
 
 class CreateNewPost(graphene.Mutation):
     class Arguments:
         title = graphene.String(required=True)
         content = graphene.String(required=True)
+
     ok = graphene.Boolean()
+
+    @staticmethod
+    def mutate(root, info, title, content):
+        post = PostSchema(title=title, content=content)
+        db_post = models.Post(title=post.title, content=post.content)
+        db.add(db_post)
+        db.commit()
+        db.refresh(db_post)
+        ok = True
+        return CreateNewPost(ok=ok)
+
 
 class PostMutations(graphene.ObjectType):
     create_new_post = CreateNewPost.Field()
 
 
-
-app.mount("/graphql",GraphQLApp(schema=graphene.Schema(),on_get=make_graphiql_handler()))
-
+app.mount("/graphql", GraphQLApp(schema=graphene.Schema(query=Query, mutation=PostMutations),on_get=make_graphiql_handler()))
--- schemas.py
+++ schemas.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel
-
-
+from graphene_sqlalchemy import SQLAlchemyObjectType
+from models import Post
 class PostSchema(BaseModel):
     title:str
     content:str
 
+class PostModel(SQLAlchemyObjectType):
+    class Meta:
+        model = Post
